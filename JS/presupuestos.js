c192fbdbb8f350e7b11504da8388b4a4a95f35b1
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-06-05 18:13:27 +0200

    Mensaje de error al no introducir valores + Restablecer valores onload
    También el comienzo de la función mostrarMovimientos.

--- JS/presupuestos.js
+++ JS/presupuestos.js
@@ -3,18 +3,39 @@ console.log("Presupuestos");
 let arrayIngresos = [];
 let arrayGastos = [];
 
+function restablecerValores (){
+    document.getElementById("descripcionMovimiento").value = "";
+    document.getElementById("dineroMovimiento").value = "";
+}
+
+function mostrarMovimientos(){
+    console.log("Funcion mostrarMovimientos");
+    const columnasMovimientos = document.getElementsByClassName("movimientosIngresos");
+    columnasMovimientos[0].innerHTML = "";
+    columnasMovimientos[1].innerHTML = "";
+}
+
 function introducirMovimiento () {
     console.log("Funcion introducirMovimiento");
     let signoSelect = document.getElementById("signo");
     let signo = signoSelect.options[signoSelect.selectedIndex].value;
+    let descripcion = document.getElementById("descripcionMovimiento").value;
+    let valor = document.getElementById("dineroMovimiento").value;
     console.log(signo);
 
-    if( signo == "positivo"){
+    if(descripcion == "" || valor == ""){
+        document.getElementById("mensajeError").innerHTML = "Introducir valores correctos";
+        setTimeout(() => document.getElementById("mensajeError").innerHTML = "", 4000);
+    }
+    else if( signo == "positivo"){
     let descripcion = document.getElementById("descripcionMovimiento").value;
     let valor = document.getElementById("dineroMovimiento").value;
 
     let movimiento = new Ingreso (descripcion, valor);
     arrayIngresos.push(movimiento);
+
+    mostrarMovimientos();
+    restablecerValores();
 
     console.log(arrayIngresos);
     }
@@ -25,6 +46,9 @@ function introducirMovimiento () {
 
     let movimiento = new Gasto (descripcion, -valor);
     arrayGastos.push(movimiento);
+
+    mostrarMovimientos();
+    restablecerValores();
 
     console.log(arrayGastos);
         
